07660d354925eed1fb7a2bedc0d4487afbc82c77
Belsons Technologies <Belsons Technologies@BT-LT-PC21.LSDXBGROUP.com>
2024-07-10 12:05:28 +0400

    Added basic news article service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewsArticleService } from './news.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  constructor(private newsArticleService: NewsArticleService) {}
+  ngOnInit(): void {
+    this.newsArticleService.getNews();
+    // const foo = new NewsArticleService();
+  }
   title = 'client';
 }
---
+++ src/app/news.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class NewsArticleService {
+  constructor(private http: HttpClient) {
+    // This service can now make HTTP requests via `this.http`.
+  }
+  async getNews() {
+    console.log('making an attempt...');
+    const x = await lastValueFrom(
+      this.http.get('http://localhost:8000/api/v1/news?q=Russia/')
+    );
+    console.log(x);
+    return x;
+  }
+}
