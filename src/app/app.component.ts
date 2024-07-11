499a25a3d19cca0c621676852e76b90ac35268ea
Taufiq Syed <tfqsyed@gmail.com>
2024-07-11 14:14:23 +0400

    Initialized pagination links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  ActivatedRoute,
+  ParamMap,
+  RouterModule,
+  RouterOutlet,
+} from '@angular/router';
 import { NewsArticleService } from './news.service';
 import { INewsArticle } from '../constants/interfaces';
 import { CommonModule } from '@angular/common';
+import { HttpParams } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
+  page$: Observable<string>;
   newsArticles: INewsArticle[] = [];
-  constructor(private newsArticleService: NewsArticleService) {}
+  constructor(
+    private newsArticleService: NewsArticleService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.newsArticleService.getNews().then((e) => {
       this.newsArticles = e;
     });
+    this.page$ = this.route.queryParamMap.pipe(
+      map((params: ParamMap) => params.get('page') ?? '1')
+    );
     // const foo = new NewsArticleService();
   }
+
   title = 'client';
 }
--- src/constants/interfaces.ts
+++ src/constants/interfaces.ts
@@ -1,5 +1,5 @@
 export interface INewsArticle {
-  author: string;
+  author?: string;
   title: string;
   description: string;
   url: string;
